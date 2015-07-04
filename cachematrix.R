78726393de9cfe5f13980a4548f7c2ebb6415135
adico <adico@twistit.co.il>
2015-07-05 01:19:30 +0300

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## matrix w/cached inverse class based on makeVector & cachemean 
 
-## Write a short comment describing this function
-
+## create a cached-inverse-matrix object, 
+## assuming passed argument x is a non-singular matrix
+## returns a list of methods to interact with the inner matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## cache time-consuming matrix-inverse computations
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
